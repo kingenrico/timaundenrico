26195ce047590f8eca584675f17a92c3dfe3afd5
kingenrico <99815624+kingenrico@users.noreply.github.com>
2022-02-16 16:28:26 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,567 @@
+input.onButtonPressed(Button.A, function () {
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(9)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # # .
+        . . . # # # # # . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . . . # # . . . .
+        . # # . # # . . . .
+        . # # . # . . . . .
+        . . # # # # # # . .
+        . . . # # # # . . .
+        `).showImage(9)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . # # . . # # . .
+        . . # # . . # # . .
+        . . . # # # . . . .
+        . . . . # # . . . .
+        . . . . . # # . . .
+        `).showImage(9)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        . . . . # . # # . .
+        `).showImage(9)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        . . . # # # # # . .
+        `).showImage(9)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        . . . # # . . . . .
+        `).showImage(9)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(5)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(6)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(7)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(8)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . . . . . .
+        . . . . . # # . . .
+        . . . # # # # . . .
+        `).showImage(9)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showNumber(input.temperature())
+        if (input.temperature() > 28) {
+            music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+            basic.showIcon(IconNames.Sad)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+})
